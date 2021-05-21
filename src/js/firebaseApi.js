a813bcc13b573a25a2ccae475b08085b7b30ac58
Iryna Dagli <dagliirina@ukr.net>
2021-05-21 16:05:11 +0300

    fix dropdown

--- src/js/firebaseApi.js
+++ src/js/firebaseApi.js
@@ -1,8 +1,10 @@
-//import 'firebaseui/dist/firebaseui.css';
+import 'firebaseui/dist/firebaseui.css';
 import * as firebaseui from 'firebaseui';
 import firebase from 'firebase';
 import 'firebase/auth';
 import 'firebase/database';
+import userProfileTmpl from '../tmpl/userProfileTmpl.hbs';
+import { signInBtnRef, authContaineRef, authWrapperRef } from './refs';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDWzddomj4Sa8oikOHu3F1SB41Hl4wPnag",
@@ -16,32 +18,25 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-export const ui = new firebaseui.auth.AuthUI(firebase.auth());
-export const uiConfig = {
-  callbacks: {
-    signInSuccessWithAuthResult: function (authResult) {
-      // alert('Access successful. HELLO KITTY!');
-      return false;
-    },
-    uiShown: function () {},
-  },
+ const ui = new firebaseui.auth.AuthUI(firebase.auth());
+const uiConfig = {
+
   signInOptions: [
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    // firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    // firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+   firebase.auth.PhoneAuthProvider.PROVIDER_ID,
   ],
-  tosUrl: 'https://www.termsfeed.com/live/255b9d74-2174-485a-b58c-eb186fe5639f',
-  // privacyPolicyUrl:
-  //   'https://www.privacypolicies.com/live/44ccb766-aaf4-4d9b-a5b3-584ef9c0a4ed',
+  
   signInFlow: 'popup',
 };
 
 ui.start('#firebaseui-auth-container', uiConfig);
 
-// https://team-project-event-booster.firebaseapp.com/__/auth/handler
 
-export const db = firebase.database();
+
+
+
+const db = firebase.database();
 
 const docRef = db.ref('clients').push();
 console.log(docRef);
@@ -56,20 +51,59 @@ async function  newClient(newClinet) {
            }
        }
 
+function dropdown(element) {
+  element.addEventListener('click', function () {
+    element.classList.toggle('active');
 
+    if (element.classList.contains('active')) {
+      signInBtnRef.addEventListener('click', function (e) {
+        closeTargetElm(e.target, element);
+      });
+    } else {
+      signInBtnRef.removeEventListener('click', function (e) {
+        closeTargetElm(e.target, element);
+      });
+    }
+  });
+}
 
-const user = firebase.auth().currentUser;
-firebase.auth().onAuthStateChanged(function (user) {
-  if (user) {
-    console.log('firebase :', user.uid);
-    return user.uid;
-    // User is signed in.
-  } else {
-    // No user is signed in.
+function closeTargetElm(target, element) {
+  if (target !== element) {
+    element.classList.remove('active');
   }
+}
+//const signInBtnRef = document.querySelector('.sign-in-button');
+dropdown(signInBtnRef);
+
+//authContaineRef.insertAdjacentHTML('afterend', userProfileTmpl());
+
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        // User is signed in.
+        // console.log(user);
+        //signInBtnRef.removeEventListener('click', dropdown);
+        //dropdown();
+        //signInBtnRef.style.display = 'none';
+        signInBtnRef.innerText = "Sign out";
+        signInBtnRef.addEventListener('click', signOutUser)
+        authContaineRef.style.display = 'none';
+      
+    }
 });
 
-console.log(user);
 
-export { user };
+function signOutUser() {
+  console.log('sign out');
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      signInBtnRef.style.display = 'flex';
+      authContaineRef.style.display = 'block';
+      signInBtnRef.addEventListener('click', dropdown);
+      location.reload();
+    })
+    //   .catch(error =>
+    //       toastr.error(error.message));
+}
 
